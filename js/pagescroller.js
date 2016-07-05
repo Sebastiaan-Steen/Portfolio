88e00d66c4f6efbc6226492c86f241c2f3895279
Sebastiaan-Steen <steensebastiaan@gmail.com>
2016-07-05 15:48:38 +0200

    it's never ok :P

--- js/pagescroller.js
+++ js/pagescroller.js
@@ -1,8 +1,43 @@
-
-
 var sections = document.getElementsByTagName("section");
-//var runAnimation = setTimeout('scroll', 24);
+var pageheight = document.documentElement.scrollHeight;
+var screenposition = window.pageYOffset;
 
+addEventListener("scroll", function () {
+    var previousposition = window.pageYOffset || document.documentElement.scrollTop;
+        
+        if (previousposition > screenposition) {
+            console.log("down");
+        //    nexttarget();
+        }
+        else {
+            console.log("up");
+            
+           // previoustarget();
+        }
+        previousposition = screenposition; 
+}, false);
+
+function nexttarget() {
+    //console.log("scrolling down");
+}
+
+function previoustarget() {
+    //console.log("scrolling up");
+}
+
+
+/*
+    if (currentposition > screenposition) {
+        console.log("scrolls up");
+    }
+    else if (currentposition < screenposition) {
+        console.log("scrolls down");
+    }
+*/
+
+
+//var runAnimation = setTimeout('scroll', 24);
+/*
 window.addEventListener("scroll", function() {
     var previousTop = window.pageYOffset;
     for (var i = 0; i < sections.length; i++) {
@@ -19,4 +54,4 @@ window.addEventListener("scroll", function() {
     }
     previousTop = st;
 }, false);
-
+*/
