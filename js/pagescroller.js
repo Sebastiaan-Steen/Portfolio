17338b777d9ddf82fec4cbd2e1d8eb444eb56e55
Sebastiaan-Steen <steensebastiaan@gmail.com>
2016-07-06 10:43:45 +0200

    added target classes and changed the JS code

--- js/pagescroller.js
+++ js/pagescroller.js
@@ -1,57 +1,21 @@
-var sections = document.getElementsByTagName("section");
+var target = document.getElementsByClassName("scrollertarget")
 var pageheight = document.documentElement.scrollHeight;
-var screenposition = window.pageYOffset;
+var previousposition = window || document.documentElement.scrollTop;
+var scrollcount = 0;
 
 addEventListener("scroll", function () {
-    var previousposition = window.pageYOffset || document.documentElement.scrollTop;
+    var screenposition = window.pageYOffset;
+
+    if (screenposition > previousposition) {
+        console.log(scrollcount);
+        scrollcount = scrollcount + 1;
+        if (scrollcount > target.length) {
+            scrollcount = target.length;
+        }
+        window.scrollTo(0, target[scrollcount].offsetTop);
+    }
+    else {
         
-        if (previousposition > screenposition) {
-            console.log("down");
-        //    nexttarget();
-        }
-        else {
-            console.log("up");
-            
-           // previoustarget();
-        }
-        previousposition = screenposition; 
+    }
+    previousposition = screenposition;
 }, false);
-
-function nexttarget() {
-    //console.log("scrolling down");
-}
-
-function previoustarget() {
-    //console.log("scrolling up");
-}
-
-
-/*
-    if (currentposition > screenposition) {
-        console.log("scrolls up");
-    }
-    else if (currentposition < screenposition) {
-        console.log("scrolls down");
-    }
-*/
-
-
-//var runAnimation = setTimeout('scroll', 24);
-/*
-window.addEventListener("scroll", function() {
-    var previousTop = window.pageYOffset;
-    for (var i = 0; i < sections.length; i++) {
-        if (st > previousTop) {
-            console.log("scrolls down");
-            var st = window || document.documentElement.scrollTop;
-          //  window.scrollTo(0, sections[i].pageYoffset);
-        }
- 
-        else {
-            console.log("scrolls up");
-        //    window.scrollTo(0, getSections[i].pageYoffset);
-        }
-    }
-    previousTop = st;
-}, false);
-*/
