f6606966086dde3d28d7ff37eef565254e48fca9
Sebastiaan-Steen <steensebastiaan@gmail.com>
2016-07-20 14:37:42 +0200

    added the snimation function

--- js/pagescroller.js
+++ js/pagescroller.js
@@ -1,36 +1,33 @@
-var target = document.getElementsByClassName("scrollertarget")
-//var pageheight = document.documentElement.scrollHeight;
-//var previousposition = window || document.documentElement.scrollTop;
-var currentOffset = window.pageYOffset;
+var target = document.getElementsByClassName("scrollertarget");
+console.log(target);
 var scrollcount = 0;
 
-
 window.onscroll = function () {
-    var newposition = window.scrollY;
-    scrollcount = scrollcount + 1;
+    var targetElementOffset = target[scrollcount].offsetTop;
+    var windowOffset = window.pageYOffset;
+
     console.log(scrollcount);
 
-    if (newposition > currentOffset) {
-
+    if (windowOffset > targetElementOffset) {
+        console.log('is scrolling down ', targetElementOffset + ' new position: ' + windowOffset);
+        scrollToSection(target[scrollcount]);
+    } else {
+        console.log('is scrolling up', targetElementOffset + ' new position: ' + windowOffset);
+        //scrollcount = scrollcount - 1;
+        scrollToSection(target[scrollcount]);
     }
-    else {
-
-    }
-    currentOffset = newposition;
-    // window.scrollTo(0, target[i]);
 }
 
-/*
-addEventListener("scroll", function () {
-
-    var screenposition = window.pageYOffset;
-    if (screenposition > previousposition) {
-        console.log(scrollcount);
-//      window.scrollTo(0, target[scrollcount].offsetTop);
+function scrollToSection(gotoSection) {
+    console.log('gotoSection: ', gotoSection);
+    var diff = (gotoSection.offsetTop - window.scrollY) / 8;
+    if (Math.abs(diff)>1) {
+        window.scrollTo(0, (window.scrollY + diff));
+        clearTimeout(window._TO);
+        window._TO = setTimeout(scrollToSection, 30, gotoSection);
+    } else {
+        window.scrollTo(0, gotoSection.offsetTop);
     }
-    else {
 
-    }
-    previousposition = screenposition;
-}, false);
-*/
+    scrollcount = scrollcount + 1;
+}
